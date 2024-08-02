97e0d048880b671ea6bead89e3d73da120d52e1d
Hari Shanker Yadav <113604005+Harishankeryadav@users.noreply.github.com>
2024-08-02 19:27:15 +0530

    Add files via upload

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BfhlData from './components/BfhlData';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BfhlData />
+      </header>
+    </div>
+  );
+}
+
+export default App;
